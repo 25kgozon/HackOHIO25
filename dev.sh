b0a05b40d618ad2629f741477b8f24517af42a9b
HeronErin <HeronErin@protonmail.com>
2025-10-25 11:16:46 -0400

    Working docker again

--- dev.sh
+++ dev.sh
@@ -1,19 +1,28 @@
 #!/bin/env sh
 set -e
 
-python backend/serve.py &
+
+sudo docker compose up db -d
+
+python src/backend/serve.py &
 BACKEND_PID=$!
-python backend/runner.py &
+python src/backend/runner.py &
 RUNNER_PID=$!
+
+
 
 cleanup() {
     echo "Stopping backend (PID: $BACKEND_PID)..."
     kill "$BACKEND_PID" 2>/dev/null || true
     
     echo "Stopping backend (PID: $RUNNER_PID)..."
-    kill "$runner_PID" 2>/dev/null || true
+    kill "$RUNNER_PID" 2>/dev/null || true
+
+    echo "Killing DB"
+    sudo docker compose down db
+
 }
 trap cleanup EXIT INT TERM
 
-cd vite-ui
+cd src/frontend/vite-ui
 npm run dev || true

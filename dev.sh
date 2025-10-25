7610270728c0253792eaf9482e61b07b6a946114
HeronErin <HeronErin@protonmail.com>
2025-10-25 14:00:33 -0400

    clear

--- dev.sh
+++ dev.sh
@@ -6,8 +6,8 @@ sudo docker compose up db -d
 
 python src/backend/serve.py &
 BACKEND_PID=$!
-python src/backend/runner.py &
-RUNNER_PID=$!
+#python src/backend/runner.py &
+#RUNNER_PID=$!
 
 
 
@@ -15,8 +15,8 @@ cleanup() {
     echo "Stopping backend (PID: $BACKEND_PID)..."
     kill "$BACKEND_PID" 2>/dev/null || true
     
-    echo "Stopping backend (PID: $RUNNER_PID)..."
-    kill "$RUNNER_PID" 2>/dev/null || true
+    #echo "Stopping backend (PID: $RUNNER_PID)..."
+    #kill "$RUNNER_PID" 2>/dev/null || true
 
     echo "Killing DB"
     sudo docker compose down db

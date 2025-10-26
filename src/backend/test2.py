005728199c526fe3825c6853220983a72f6d76fb
HeronErin <HeronErin@protonmail.com>
2025-10-25 23:20:30 -0400

    m

--- src/backend/test2.py
+++ src/backend/test2.py
@@ -15,5 +15,5 @@ import requests
 db: DB = DB()
 
 
-print(db.create_assignment(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7"), "foo ass", "desc", {}, {}, ""))
-print(db.get_class_assignments(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7")))
+# print(db.create_assignment(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7"), "foo ass", "desc", {}, {}, ""))
+# print(db.get_class_assignments(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7")))

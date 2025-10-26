81c28edad71526755555421d354f1609b7146835
HeronErin <HeronErin@protonmail.com>
2025-10-26 00:34:27 -0400

    testing

--- src/backend/test2.py
+++ src/backend/test2.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 
-
+from uuid import UUID
 
 
 from db import *
@@ -17,3 +17,9 @@ db: DB = DB()
 
 # print(db.create_assignment(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7"), "foo ass", "desc", {}, {}, ""))
 # print(db.get_class_assignments(UUID("3829cf32-2a91-4eeb-a7f7-68a622d485f7")))
+
+
+db.enqueue_text_task(TaskType.SUMMARIZE, [], [UUID("142f668d-8556-40b7-a0a6-7bb7a4bbc053")], "")
+print(db.dequeue_text_task())
+
+
